7b5cd77f5fb1c519c09a812ff6ded63804e863b3
Katelin Francois <katelinfrancois@gmail.com>
2021-06-20 03:38:02 -0500

    finished JS

---
+++ scripts/index.js
@@ -0,0 +1,27 @@
+let editBtn = document.querySelector("#editBtn");
+let overlay = document.querySelector(".overlay");
+let modal = document.querySelector(".modal");
+let profileName = document.querySelector(".profile__name");
+let profileTitle = document.querySelector(".profile__title");
+let nameInput = document.forms.profile.elements.name;
+let titleInput = document.forms.profile.elements.title;
+
+editBtn.addEventListener("click", function (event) {
+  nameInput.value = profileName.textContent;
+  titleInput.value = profileTitle.textContent;
+  overlay.classList.add("overlay_show");
+  modal.classList.add("modal__show");
+});
+
+closeBtn.addEventListener("click", closeModal);
+
+saveBtn.addEventListener("click", function () {
+  profileName.textContent = nameInput.value;
+  profileTitle.textContent = titleInput.value;
+  closeModal();
+});
+
+function closeModal() {
+  overlay.classList.remove("overlay__show");
+  modal.classList.remove("modal__show");
+}
