fbda2110baa4b5f1dbf3a3cd6cd6c89418b93bab
Katelin Francois <katelinfrancois@gmail.com>
2021-06-22 23:15:09 -0500

    project update...fixed modal layout

--- README.md
+++ README.md
@@ -1,16 +1,5 @@
 # Project 4: Around The U.S.
 
-### Overview
+This is a fun project showing an example of a social media like profile page. I used JavaScript to program the "edit" button to have an edit page pop up so that the user can change their profile bio. 
 
-* Figma
-* Images
-
-**Figma**
-
-* [Link to the project in Figma](https://www.figma.com/file/SurN1jaeEQIhuZEDMhmWWf/Sprint-4-Around-The-U.S.-desktop-mobile?node-id=0%3A1)
-
-**Images**
-
-The way you'll do this at work is by exporting images directly from Figma — we recommend doing that to practice more. Don't forget to optimize them [here](https://tinypng.com/), so your project loads faster. 
-
-Have fun with JavaScript!
+I have my project published here --->   https://katelinfrancois.github.io/web_project_4/ 
--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 let editBtn = document.querySelector("#editBtn");
-let overlay = document.querySelector(".overlay");
+let overlay = document.querySelector(".modal__overlay");
 let modal = document.querySelector(".modal");
 let profileName = document.querySelector(".profile__name");
 let profileTitle = document.querySelector(".profile__title");
@@ -9,7 +9,7 @@ let titleInput = document.forms.profile.elements.title;
 editBtn.addEventListener("click", function (event) {
   nameInput.value = profileName.textContent;
   titleInput.value = profileTitle.textContent;
-  overlay.classList.add("overlay_show");
+  overlay.classList.add("modal__overlay_show");
   modal.classList.add("modal_show");
 });
 
@@ -22,6 +22,6 @@ saveBtn.addEventListener("click", function () {
 });
 
 function closeModal() {
-  overlay.classList.remove("overlay_show");
+  overlay.classList.remove("modal__overlay_show");
   modal.classList.remove("modal_show");
 }
